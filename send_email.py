a2b3b4823678c946347a945abda18792f25d1a87
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-06-08 15:43:19 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -64,6 +64,7 @@ if __name__ == '__main__':
         os.getenv('TO_EMAIL13'), 
         os.getenv('TO_EMAIL14'),
         os.getenv('TO_EMAIL15'), 
+        os.getenv('TO_EMAIL16'), 
         os.getenv('TO_EMAIL1'),
         os.getenv('TO_EMAIL8'), 
         os.getenv('TO_EMAIL9'), 
